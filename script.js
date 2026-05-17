6b5e152863000d26dc790242c8f726736a84586c
lucasfiotti <sfiottiluca@gmail.com>
2026-05-18 01:31:48 +0200

    Changement du fetch (mtn synchrone) ajout de la section tendances avec affichage grâce au js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const API_KEY = '33a0dd3dbdda91ac44cfa3af14f24f03';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
-fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&language=fr-FR`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+async function afficherTendances() {
+    const response = await fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&language=fr-FR`);
+    const data = await response.json();
+
+    const grid = document.getElementById('actuTendances');
+
+    data.results.forEach(film => {
+        const carte = document.createElement('article');
+        carte.innerHTML = `
+      <img src="${IMG_URL}${film.poster_path}" alt="${film.title || film.name}"/>
+      <p>${film.title || film.name}</p>
+    `;
+        grid.appendChild(carte);
     });
+}
+
+afficherTendances();
