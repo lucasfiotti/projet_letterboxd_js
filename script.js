1da1abe457df262461c01bc48c0b357f162f23fa
lucasfiotti <sfiottiluca@gmail.com>
2026-05-18 19:23:39 +0200

    Ajout de la section séries avec début d'affichage des séries populaires

--- script.js
+++ script.js
@@ -24,4 +24,30 @@ async function afficherTendances() {
     });
 }
 
+async function afficherSeries() {
+    const response = await fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}&language=fr-FR`);
+    const data = await response.json();
+
+    const grid = document.getElementById('series-grid');
+
+    data.results.slice(0, 8).forEach(serie => {
+        const carte = document.createElement('article');
+        carte.innerHTML = `
+      <div class="poster-wrapper">
+        <img src="${IMG_URL}${serie.poster_path}" alt="${serie.name}"/>
+        <span class="score">${Math.round(serie.vote_average * 10)}%</span>
+      </div>
+      <p class="titre">${serie.name}</p>
+      <p class="date">${new Date(serie.first_air_date).toLocaleDateString('fr-FR', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric'
+        })}</p>
+    `;
+        grid.appendChild(carte);
+    });
+}
+
 afficherTendances();
+afficherSeries();
+
